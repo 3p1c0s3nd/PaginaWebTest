0656a8abf37124beb05b2814672d43cf374635ba
3p1c0w3nd <3p1c0w3nd@1337.com>
2023-08-25 13:31:06 -0500

    Se agrego para que funcione con plantillas

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const express = require('express')
-const app = express()
-const puerto = 9000;
-app.use(express.static(__dirname + "public/")); //Esto es para publicar solo los archivos de la carpeta public
+const express = require('express');
+const { create } = require("express-handlebars");
 
-app.get((res, req) =>{
-    res.send("Hola mundo desde el index.js");
+const app = express();
+const puerto = 9000;
+const hbs = create({
+    extname: ".hbs",
 });
 
+//Configurar el motor de plantillas
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
+app.set("views", "./views");
+
+
+app.get("/", (req, res) =>{
+    res.render("home", {titulo: "Titulo Home"});
+});
+
+app.get("/login", (req, res) => {
+    res.render("login", {titulo: "Titulo Login"});
+});
+
+app.use(express.static(__dirname + "/public")); //Esto es para publicar solo los archivos de la carpeta public
 app.listen(puerto, () => console.log("Servidor iniciado en el puerto "+ puerto));
